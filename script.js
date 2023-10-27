e72aa4125319642d3f32f279b6010dbd59e74dc1
Lunastellia <79289200+Lunastellia@users.noreply.github.com>
2023-10-27 14:20:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Premier ensemble de menus déroulants
-    const imageElement = document.getElementById("image");
     const selectorElement = document.getElementById("image-selector");
 
     // Deuxième ensemble de menus déroulants
-    const imageElement2 = document.getElementById("image2");
     const selectorElement2 = document.getElementById("image-selector2");
 
     // Valeurs Ilevel global, Valeur offensive et Valeur defensive
@@ -14,15 +12,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Fonction pour mettre à jour les valeurs
     function updateValues() {
-        const selectedValue1 = parseInt(selectorElement.value, 10);
-        const selectedValue2 = parseInt(selectorElement2.value, 10);
+        const selectedValue1 = parseInt(selectorElement.options[selectorElement.selectedIndex].getAttribute("data-value"), 10);
+        const selectedValue2 = parseInt(selectorElement2.options[selectorElement2.selectedIndex].getAttribute("data-value"), 10);
 
-        const additionalValue1 = parseInt(selectorElement.options[selectorElement.selectedIndex].getAttribute("data-value"), 10);
-        const additionalValue2 = parseInt(selectorElement2.options[selectorElement2.selectedIndex].getAttribute("data-value"), 10);
-
-        ilevelValueElement.textContent = additionalValue2 + additionalValue1;
-        offensiveValueElement.textContent = selectedValue1 + additionalValue1;
-        defensiveValueElement.textContent = selectedValue2 + additionalValue2;
+        ilevelValueElement.textContent = selectedValue1 + selectedValue2;
+        offensiveValueElement.textContent = selectedValue1;
+        defensiveValueElement.textContent = selectedValue2;
     }
 
     // Gestionnaires d'événements pour les menus déroulants
