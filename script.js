bcc8ec8d8d563abfa5c3f0eebccc0d6e2428b107
Lunastellia <79289200+Lunastellia@users.noreply.github.com>
2023-10-27 14:30:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Premier ensemble de menus déroulants
+    const imageElement = document.getElementById("image");
     const selectorElement = document.getElementById("image-selector");
 
     // Deuxième ensemble de menus déroulants
+    const imageElement2 = document.getElementById("image2");
     const selectorElement2 = document.getElementById("image-selector2");
 
     // Valeurs Ilevel global, Valeur offensive et Valeur defensive
@@ -14,10 +16,16 @@ document.addEventListener("DOMContentLoaded", function () {
     function updateValues() {
         const selectedValue1 = parseInt(selectorElement.options[selectorElement.selectedIndex].getAttribute("data-value"), 10);
         const selectedValue2 = parseInt(selectorElement2.options[selectorElement2.selectedIndex].getAttribute("data-value"), 10);
+        const imageSrc1 = selectorElement.options[selectorElement.selectedIndex].getAttribute("data-image");
+        const imageSrc2 = selectorElement2.options[selectorElement2.selectedIndex].getAttribute("data-image");
 
         ilevelValueElement.textContent = selectedValue1 + selectedValue2;
         offensiveValueElement.textContent = selectedValue1;
         defensiveValueElement.textContent = selectedValue2;
+
+        imageElement.src = imageSrc1;
+        imageElement2.src = imageSrc2;
+        
     }
 
     // Gestionnaires d'événements pour les menus déroulants
