4176b1547a26aba8ee2671a5d0ff7a1f556fbb96
Lunastellia <79289200+Lunastellia@users.noreply.github.com>
2023-10-27 13:47:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-document.addEventListener("DOMContentLoaded", function ()
-                        
-{
-    // 
-    const imageElement = document.getElementById("item_head");
-    const selectorElement = document.getElementById("item_head-selector");
-    const selectedValueElement = document.getElementById("item_head-selectedvalue");
+document.addEventListener("DOMContentLoaded", function () {
+
+  
+    // Premier ensemble de menus déroulants
+    const imageElement = document.getElementById("image");
+    const selectorElement = document.getElementById("image-selector");
+    const selectedValueElement = document.getElementById("selected-value");
 
     selectorElement.addEventListener("change", function () {
         const selectedValue = selectorElement.value;
@@ -12,20 +12,39 @@ document.addEventListener("DOMContentLoaded", function ()
 
         switch (selectedValue) {
             case "0":
-                imageElement.src = "https://github.com/Lunastellia/ilevel/blob/main/pic/head0.png";
+                imageElement.src = "pic/head0.png";
                 break;
             case "1":
-                imageElement.src = "https://github.com/Lunastellia/ilevel/blob/main/pic/head0.png";
+                imageElement.src = "pic/head1.png";
                 break;
             case "2":
-                imageElement.src = "https://github.com/Lunastellia/ilevel/blob/main/pic/head0.png";
+                imageElement.src = "pic/head2.png";
                 break;
             case "3":
-                imageElement.src = "https://github.com/Lunastellia/ilevel/blob/main/pic/head0.png";
+                imageElement.src = "pic/head3.png";
                 break;
         }
     });
 
-    
+    // Deuxième ensemble de menus déroulants
+    const imageElement2 = document.getElementById("image2");
+    const selectorElement2 = document.getElementById("image-selector2");
+    const selectedValueElement2 = document.getElementById("selected-value2");
+
+    selectorElement2.addEventListener("change", function () {
+        const selectedValue = selectorElement2.value;
+        selectedValueElement2.textContent = `Selected Value: ${selectedValue}`;
+
+        switch (selectedValue) {
+            case "5":
+                imageElement2.src = "div/truc.png";
+                break;
+            case "10":
+                imageElement2.src = "div/bidule.png";
+                break;
+        }
+    });
+
+  
 });
 
