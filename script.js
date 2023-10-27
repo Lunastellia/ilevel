717b7713bddb6f1c07abb30a46a018bd039882c4
Lunastellia <79289200+Lunastellia@users.noreply.github.com>
2023-10-27 13:03:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
         switch (selectedValue) {
             case "0":
-                imageElement.src = "pic/head0.png";
+                imageElement.src = "https://github.com/Lunastellia/ilevel/blob/main/pic/head0.png";
                 break;
             case "1":
-                imageElement.src = "pic/head1.png";
+                imageElement.src = "https://github.com/Lunastellia/ilevel/blob/main/pic/head0.png";
                 break;
             case "2":
-                imageElement.src = "pic/head2.png";
+                imageElement.src = "https://github.com/Lunastellia/ilevel/blob/main/pic/head0.png";
                 break;
             case "3":
-                imageElement.src = "pic/head3.png";
+                imageElement.src = "https://github.com/Lunastellia/ilevel/blob/main/pic/head0.png";
                 break;
         }
     });
